05a02e74d07949e8b551347a974a31456d0cd8e3
jmaushake <joerg@maushake.de>
2014-06-27 14:33:54 +0200

    exclude CredentialServiceFactoryTest from test

--- test/com/doctainer/credsrv/CredentialServiceFactoryTestX.java
+++ test/com/doctainer/credsrv/CredentialServiceFactoryTestX.java
@@ -3,11 +3,12 @@ package com.doctainer.credsrv;
 import java.io.IOException;
 import java.util.Map;
 
-public class CredentialServiceFactoryTest {
+public class CredentialServiceFactoryTestX {
 	// very, very old stuff
 	public static void main(String[] args) throws IOException {
 		  CredentialService cs = CredentialServiceFactory.getCredentialService();
 		  Map<String, String> result = cs.getCredentials("PRODL");
 		  System.out.println(result);
-		 }
+	}
+	
 }
